27705b2a93aceb2c77d451bc6023683d745d3949
cristianoCololo <cristianocololo8@gmail.com>
2024-06-11 07:40:51 -0300

    04h:07min

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/fontes.css">
+    <link rel="stylesheet" href="./assets/css/index_style.css">
     <title>Sol::CriarContaOnline</title>
 </head>
+
 <body>
     <header>
         <nav>
@@ -18,9 +22,25 @@
         </nav>
     </header>
     <main>
-        <div class="left_content"></div>
-        <div class="right_content"></div>
+        <div class="left_content">
+            <p>Bem-vindo ao nosso portal de abertura de contas online, aqui,
+                você pode abrir sua conta de maneira rápida, fácil,
+                sem enfrentar filas e sem burocracia, tudo a partir do conforto
+                da sua casa. </p>
+            <p>Adira Ja</p>
+            <button id="aderir">Aderir</button>
+        </div>
+        <div class="right_content">
+            <img src="./assets/img/main.png" alt="">
+        </div>
     </main>
     <footer></footer>
+    <script>
+        const aderir = document.getElementById('aderir');
+        aderir.addEventListener('click', ()=>{
+            window.location.href = 'aderir.php';
+        })
+    </script>
 </body>
+
 </html>
