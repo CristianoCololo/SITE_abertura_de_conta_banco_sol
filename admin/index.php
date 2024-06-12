f897a17b044b5b3e33057aa56e71d9ff6c93d300
cristianoCololo <cristianocololo8@gmail.com>
2024-06-12 10:03:23 -0300

    ninja descolado 4 ; fico gang gang ; com as kunais do Minato

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../site/assets/css/destyle.css">
+    <link rel="stylesheet" href="../site/assets/css/fontes.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Admin</title>
+</head>
+<body>
+    <header>
+        <h1>Logar Como Administrador</h1>
+        <p>Insira o seu <label for="email">email</label> e <label for="password">password</label> de administrador</p>
+    </header>
+    <main>
+        <div class="form">
+            <div class="input_email">
+                <label for="email">Email</label>
+                <input type="email" id="email">
+            </div>
+            <div class="input_password">
+                <label for="password">Password</label>
+                <input type="email" id="password">
+            </div>
+        </div>
+    </main>
+    <footer>
+        <p>Sair</p>
+    </footer>
+</body>
+</html>
